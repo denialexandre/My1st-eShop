0fac381b8e714e93b708ab9261e7e88197e94a74
denialexandre <deni.alexandre.garc@gmail.com>
2022-08-10 14:41:52 -0500

    tarea10082022

--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -1,9 +1,20 @@
+import './CartWidget.css'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { CartContext } from '../../Context/CartContext'
+
 const CartWidget = () => {
-    return (
-        <div>
-            <img src='Images/cart.svg' alt="ShoppingBag" />
-        </div>
-    )
+    const { getQuantity } = useContext(CartContext)
+
+    const quantity = getQuantity()
+
+
+    return(
+        <Link to='/cart' className="CartWidget">
+            <img src="/Images/cart.svg" alt='ShoppingBag' className='CartImg'/>
+            {quantity}
+        </Link>
+    );
 }
 
 export default CartWidget
